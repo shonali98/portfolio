fdf15e735fb3ffeb8a13183ac9c9217803698747
shonali98 <shonalinethmika998@gmail.com>
2022-06-23 21:54:59 +0530

    img commit

--- src/Component/Projects/Cards.js
+++ src/Component/Projects/Cards.js
@@ -3,10 +3,10 @@ import '../Projects/Projects.css'
 export const Cards = (props) => {
   return (
     < >
-        <div className='box btn_shadow '>
+        <div className='box btn_shadow  '>
             <img src={props.image}   alt=''/>
             <h2>{props.title}</h2>
-            <p>{props.desc}</p>
+            {/* <p>{props.desc}</p> */}
 
             <a href=''>
                 <i className='fas fa-arrow-right'></i>
